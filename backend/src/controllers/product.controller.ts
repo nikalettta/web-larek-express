1cca5c7ee8985d34b8b94fb5cfa9fcdbf602a4cd
Nikakos <sleepyprincess498@gmail.com>
2026-09-19 17:40:44 +0300

    fixed product controller

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -4,7 +4,7 @@ import Product from '../models/product.model';
 import ConflictError from '../errors/conflict-error';
 import BadRequestError from '../errors/bad-request-error';
 
-const createProduct = (req: Request, res: Response, next: NextFunction) => {
+export const createProduct = (req: Request, res: Response, next: NextFunction) => {
   const {
     title,
     description,
@@ -34,4 +34,8 @@ const createProduct = (req: Request, res: Response, next: NextFunction) => {
     });
 };
 
-export default createProduct;
+export const getAllProducts = (_req: Request, res: Response, next: NextFunction) => {
+  Product.find({})
+    .then((products) => res.send({ items: products, total: products.length }))
+    .catch((err) => next(err));
+};
