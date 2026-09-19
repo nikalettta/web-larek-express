c424d51c89e4c4cf73d515148aff07423185e02a
Nikakos <sleepyprincess498@gmail.com>
2026-09-19 14:09:04 +0300

    fixed EsLint problem

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,11 +1,17 @@
-import { Request, Response, NextFunction } from "express";
-import { Error as MongooseError } from "mongoose";
-import Product from "../models/product.model";
-import ConflictError from "../errors/conflict-error";
-import BadRequestError from "../errors/bad-request-error";
+import { Request, Response, NextFunction } from 'express';
+import { Error as MongooseError } from 'mongoose';
+import Product from '../models/product.model';
+import ConflictError from '../errors/conflict-error';
+import BadRequestError from '../errors/bad-request-error';
 
 const createProduct = (req: Request, res: Response, next: NextFunction) => {
-  const { title, description, image, category, price } = req.body;
+  const {
+    title,
+    description,
+    image,
+    category,
+    price,
+  } = req.body;
 
   return Product.create({
     title,
@@ -16,9 +22,9 @@ const createProduct = (req: Request, res: Response, next: NextFunction) => {
   })
     .then((product) => res.status(201).send(product))
     .catch((err) => {
-      if (err instanceof Error && err.message.includes("E11000")) {
+      if (err instanceof Error && err.message.includes('E11000')) {
         return next(
-          new ConflictError("Товар с таким названием уже существует"),
+          new ConflictError('Товар с таким названием уже существует'),
         );
       }
       if (err instanceof MongooseError.ValidationError) {
--- backend/src/middlewares/not-found.ts
+++ backend/src/middlewares/not-found.ts
@@ -1,8 +1,8 @@
-import { Request, Response, NextFunction } from "express";
-import NotFoundError from "../errors/not-found-error";
+import { Request, Response, NextFunction } from 'express';
+import NotFoundError from '../errors/not-found-error';
 
 const notFoundHandler = (_req: Request, _res: Response, next: NextFunction) => {
-  next(new NotFoundError("Маршрут не найден"));
+  next(new NotFoundError('Маршрут не найден'));
 };
 
 export default notFoundHandler;
